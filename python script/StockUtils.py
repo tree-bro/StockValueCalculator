4055ccc376cacfaf3a3ad98bf15f8f3ff7a9f273
hx <hx>
2017-09-13 20:02:59 +0800

    Use HTMLParser instead of lxml to make it runnable on qpython

--- python script/StockUtils.py
+++ python script/StockUtils.py
@@ -5,13 +5,83 @@ import InputUtils
 from StockEntity import StockEntity
 from StockMarketTypes import StockMarketType
 from html.parser import HTMLParser
-from lxml import etree
 from decimal import Decimal
 
 stockURLTemplate = 'https://gupiao.baidu.com/stock/[_STOCK_ID_].html'
 szStockPattern = r'^00[0-9]{4}$'
 shStockPattern = r'^60[0-9]{4}$'
 hkStockPattern = r'^0[0-9]{4}$'
+
+class ResponseHtmlParser(HTMLParser):
+	parseNameNode = False
+	parseDateNode = False
+	parseClosePriceNode = False
+	parseDetailNode = False
+	parsePERatioNode = False
+	parseProfitPerSharingNode = False
+	peRatioContentCount = 0
+	nameNodeText = ''
+	dateNodeText = ''
+	closePriceNodeText = ''
+	peRatioNodeText = ''
+	profitPerSharingNodeText = ''
+
+	def handle_starttag(self, tag, attrs):
+		if (tag == 'a'):
+			for attr in attrs:
+				if (attr[0] == 'class' and attr[1] == 'bets-name'):
+					#Encountered name node
+					self.parseNameNode = True
+		elif (tag == 'span'):
+			for attr in attrs:
+				if (attr[0] == 'class' and attr[1] == 'state f-up'):
+					#Encountered date node
+					self.parseDateNode = True
+		elif (tag == 'strong'):
+			for attr in attrs:
+				if (attr[0] == 'class' and attr[1] == '_close'):
+					#Encountered close node
+					self.parseClosePriceNode = True
+		elif (tag == 'div'):
+			for attr in attrs:
+				if (attr[0] == 'class' and attr[1] == 'bets-content'):
+					self.parseDetailNode = True
+
+	def handle_endtag(self,tag):
+		if (tag == 'a' and self.parseNameNode):
+			#Encountered name node
+			self.parseNameNode = False
+		elif (tag == 'span' and self.parseDateNode):
+			#Encountered date node
+			self.parseDateNode = False
+		elif (tag == 'strong' and self.parseClosePriceNode):
+			#Encountered close node
+			self.parseClosePriceNode = False
+		elif (tag == 'div' and self.parseDetailNode):
+			#Encountered detail node
+			self.parseDetailNode = False
+
+	def handle_data(self,data):
+		if(self.parseNameNode):
+			self.nameNodeText = data
+		elif(self.parseDateNode):
+			self.dateNodeText = data
+		elif(self.parseClosePriceNode):
+			self.closePriceNodeText = data
+		elif(self.parseDetailNode):
+			if (data == '市盈率'):
+				self.parsePERatioNode = True
+			elif (data == '每股收益'):
+				self.parseProfitPerSharingNode = True
+			elif (self.parsePERatioNode):
+				if (self.peRatioContentCount < 1):
+					self.peRatioContentCount += 1
+				else:
+					self.peRatioNodeText = data
+					self.parsePERatioNode = False
+			elif (self.parseProfitPerSharingNode):
+				self.profitPerSharingNodeText = data
+				self.parseProfitPerSharingNode = False
 
 def constructURL(stockID):
 	if(re.compile(szStockPattern).match(stockID)):
@@ -29,22 +99,21 @@ def parseFromServer(stockID):
 	if (stockURL != ''):
 		response_data = urllib.request.urlopen(stockURL)
 		response = response_data.read().decode('utf-8')
-		page = etree.HTML(response)
-		entity = StockEntity()
-		nameNode = page.xpath('//a[@class=\'bets-name\']')
-		dateNode = page.xpath('//span[@class=\'state f-up\']')
-		closePriceNode = page.xpath('//strong[@class=\'_close\']')
-		detailNode = page.xpath('//div[@class=\'bets-content\']//div')
-		entity.lastTradingPrice = Decimal(closePriceNode[0].text)
-		entity.stockName = nameNode[0].text
-		entity.updateDate = dateNode[0].text
+		parser = ResponseHtmlParser()
+		parser.feed(response)
 
-		for ele in detailNode[0]:
-			if(len(ele) > 1):
-				if(ele[0].text == '市盈率'):
-					entity.peRatio = Decimal(ele[1].text)
-				elif(ele[0].text == '每股收益'):
-					entity.profitPerShareLatest = Decimal(ele[1].text)
+		entity = StockEntity()
+		entity.lastTradingPrice = Decimal(parser.closePriceNodeText)
+		print ('[公司名称='+parser.nameNodeText+']')
+		print ('[资料更新日期='+parser.dateNodeText+']')
+		print ('[市盈率=' + parser.peRatioNodeText+']')
+		print ('[每股盈利=' + parser.profitPerSharingNodeText+']')
+		entity.stockName = parser.nameNodeText
+		entity.updateDate = parser.dateNodeText
+		if (parser.peRatioNodeText != ''):
+			entity.peRatio = Decimal(parser.peRatioNodeText)
+		if (parser.profitPerSharingNodeText != ''):
+			entity.profitPerShareLatest = Decimal(parser.profitPerSharingNodeText)
 
 		return entity 
 
