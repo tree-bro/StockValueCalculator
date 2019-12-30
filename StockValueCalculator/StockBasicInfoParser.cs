fc503accf209e73994e8e8a7e43bddd34eb36a12
saligia <being.once@gmail.com>
2019-12-30 15:44:52 +0800

    Adjust the logic for parsing value from remote api

---
+++ StockValueCalculator/StockBasicInfoParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockValueCalculator
+{
+    public class StockBasicInfoParser
+    {
+        public int errorNo;
+        public string errorMsg;
+        public ExchangeStatusData exchangeStatus;
+        public SnapShotData snapShot;
+        
+    }
+}
